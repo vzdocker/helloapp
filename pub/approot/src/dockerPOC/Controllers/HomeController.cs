75ad1908f44fa41ba1ef6415b4eb427a6d95d257
unknown <z821197@MACDJENDRW01V.ctlab.verizon.com>
2016-02-02 06:00:43 -0600

    added published files

---
+++ pub/approot/src/dockerPOC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace dockerPOC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.Write("qw");
+
+            var dd = Request.Host.Value;
+            var ll = dd.Split(':');
+            ViewData["IpAddr"] = ll[0];
+            ViewData["Port"] = ll[1];
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
